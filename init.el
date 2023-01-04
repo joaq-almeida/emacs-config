d0f3610ca1e933b4069a5bd3a54a5ac4a17274f7
steve <joaquimrafael30@hotmail.com>
2023-01-03 23:43:40 -0300

    added new modes for python

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
 
 ;; MELPA -> repo
 (add-to-list 'package-archives
-	     '("melpa". "https://melpa.org/packages/"))
+	     '("melpa". "https://melpa.org/packages/") t)
 
 (package-initialize) 
 (unless (package-installed-p 'use-package)
@@ -57,10 +57,10 @@
 
 
 ;; Install Markdown mode
-(use-package markdown-mode
-  :ensure t
-  :mode ("README\\.md\\'" . gfm-mode)
-  :init (setq markdown-command "markdown"))
+;; (use-package markdown-mode
+;;   :ensure t
+;;   :mode ("README\\.md\\'" . gfm-mode)
+;;   :init (setq markdown-command "markdown"))
 
 (use-package try
   :ensure t)
@@ -114,13 +114,50 @@
   :init
   (advice-add 'python-mode :before 'elpy-enable))
 
+;;Install flycheck
+(use-package flycheck
+  :ensure t)
+
 ;; Enable elpy
 (elpy-enable)
+
+(use-package ein
+  :ensure t)
+
+;; Run autopep8 on save
+(use-package py-autopep8
+  :config
+  (setq py-autopep8-options '("--max-line-length=100" "--aggressive")))
+ ;; :hook ((elpy-mode-hook) . py-autopep8-mode))
+(add-hook 'elpy-mode-hook 'py-autopep8-mode)
+
+;; Black formatting on save
+(use-package blacken
+  :ensure t)
+
+;; Use IPython for REPL
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters "jupyter")
 
 ;; Enable Flycheck
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Enable autopep8
+;; (require 'py-autopep8)
+;; (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+;;======================================================================
+;; Perl configs.
+;;======================================================================
+
+;; (fset 'perl-mode 'cperl-mode)
+;; (setq cperl-invalid-face nil)
+;; (setq cperl-indent-parens-as-block t)
+;; ;;(setq cperl-close-paren-offset (- cperl-indent-level))
 
 ;;======================================================================
 ;; key shortcuts.
@@ -137,15 +174,6 @@
 (global-set-key [f8] 'neotree-toggle)
 (global-set-key [f9] 'neotree-dir)
 (global-set-key (kbd "M-o") 'ace-window)
-
-;;======================================================================
-;; Perl configs.
-;;======================================================================
-
-(fset 'perl-mode 'cperl-mode)
-(setq cperl-invalid-face nil)
-(setq cperl-indent-parens-as-block t)
-;;(setq cperl-close-paren-offset (- cperl-indent-level))
 
 ;;======================================================================
 ;; Themes load
@@ -168,7 +196,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elixir-mode elpy web-mode markdown-mode zenburn-theme which-key use-package try subatomic256-theme subatomic-theme spacemacs-theme solarized-theme projectile neotree moe-theme magit helm gruvbox-theme gruber-darker-theme gotham-theme ergoemacs-mode dracula-theme color-theme-sanityinc-tomorrow auto-complete all-the-icons ace-window)))
+   '(py-autopep8 elixir-mode elpy web-mode markdown-mode zenburn-theme which-key use-package try subatomic256-theme subatomic-theme spacemacs-theme solarized-theme projectile neotree moe-theme magit helm gruvbox-theme gruber-darker-theme gotham-theme ergoemacs-mode dracula-theme color-theme-sanityinc-tomorrow auto-complete all-the-icons ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
